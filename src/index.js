5481727687d2d8975cbab179f5924ce680b01471
Susep Dev <susep.dev@gmail.com>
2024-02-19 11:01:48 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+
+const mainRoutes = require('./routes'); 
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use('/api', mainRoutes);
+
+app.get('/', async (req, res) => {
+    res.send('ExpressJS');
+});
+
+app.listen(port, () => {
+    console.log(`Running on port:${port}`);
+});
