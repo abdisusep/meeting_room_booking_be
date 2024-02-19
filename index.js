047a7d311998c17f965679f1fa8ddcb3cb46390c
Susep Dev <susep.dev@gmail.com>
2024-02-19 16:48:36 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const mainRoutes = require('./routes'); 
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cors());
 
 app.use('/api', mainRoutes);
 
